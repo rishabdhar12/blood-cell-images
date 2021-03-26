5296716612b0b8a0a65c71b9e40ac7005afb26f9
Rishab Dhar <rishabdhar12@gmail.com>
2021-03-26 10:03:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,66 @@
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, render_template
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from tensorflow.keras.models import Sequential, load_model
+from werkzeug.utils import secure_filename
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+# 'file://null'
+
+
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
+IMAGE_SIZE = (224, 224)
+UPLOAD_FOLDER = 'uploads'
+model = load_model('/home/rishab/Desktop/BLOOD-CELL-IMAGES/model_2')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def predict(file):
+    final = []
+    img  = load_img(file, target_size=IMAGE_SIZE)
+    img = img_to_array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+    result = np.argmax(model.predict(img), axis=1)
+
+    if result[0] == 0:
+        return 'EOSINOPHIL'
+    elif result[0] == 1:
+        return 'LYMPHOCYTE'
+    elif result[0] == 2:
+        return 'MONOCYTE'
+    else:
+        return 'NEUTROPHIL'
+
+app = Flask(__name__, template_folder='Templates')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/")
+def template_test():
+    return render_template('home.html', label='')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            prediction = predict(file_path)
+    return render_template("home.html", label=prediction, imagesource=file_path)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == "__main__":
+    app.run(threaded=False, debug=True)
